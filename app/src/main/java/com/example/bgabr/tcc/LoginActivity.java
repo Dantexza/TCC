ac1e8a1deaa85d80249ce4efaeef7992217a4b2a
Dantexza <bgabrieldasilva2@gmail.com>
2018-09-27 18:24:27 -0300

    Intent added

--- app/src/main/java/com/example/bgabr/tcc/LoginActivity.java
+++ app/src/main/java/com/example/bgabr/tcc/LoginActivity.java
@@ -1,13 +1,25 @@
 package com.example.bgabr.tcc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final Intent intent = new Intent(this,MainActivity.class);
         setContentView(R.layout.activity_login);
+        Button  btnlogin = (Button)findViewById(R.id.btnLogin);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+
+            }
+        });
     }
 }
